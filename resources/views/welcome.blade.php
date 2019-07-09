a7c0ab693c760b26188c0bc1063c412fb8083d23
Nikola <milje18@gmail.com>
2019-07-09 21:50:38 +0200

    folder structure with models and migrations and database seeder

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,47 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <h1>Recent jobs</h1>
+      <table class="table">
+          <thead>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+              <th></th>
+          </thead>
+          <tbody>
+              @foreach($jobs as $job)
+                <tr>
+                    <td><img src="{{asset('avatar/man.jpg')}}" width="80"></td>
+                    <td>Position: {{$job->position}}
+                        <br>
+                        <i class="fa fa-clock-o" aria-hidden="true"></i>
+                       &nbsp;{{$job->type}}
+                    </td>
+                    <td>
+                        <i class="fa fa-map-marker" aria-hidden="true"></i>
+                    &nbsp;Address:{{$job->address}}
+                    </td>
+                    <td>
+                        <i class="fa fa-globe" aria-hidden="true"></i>
+                        &nbsp;Date: {{$job->created_at->diffForHumans()}}
+                    </td>
+                    <td>
+                        <button class="btn btn-success btn-sm">Apply</button>
+                    </td>  
+                </tr>
+              @endforeach
+          </tbody>
+      </table>
+    </div>
+</div>
+@endsection
+
+<style>
+.fa{
+    color: #4183D7;
+}
+</style>
